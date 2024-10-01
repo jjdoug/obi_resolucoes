95f107b0eba36fffad926d222760594b7685e1bd
Jefferson Douglas <jedouglas.study@gmail.com>
2024-09-30 22:56:40 -0300

    Update alfabeto_alienigena.py

--- 2024/fase02/alfabeto_alienigena.py
+++ 2024/fase02/alfabeto_alienigena.py
@@ -1,1 +1,11 @@
-print('a')
+k, n = map(int, input().split())
+alfabeto = input()
+msg = input()
+
+resposta = 'S'
+for caractere in msg:
+    if caractere not in alfabeto:
+        resposta = 'N'
+        break
+
+print(resposta)
