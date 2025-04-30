a7cb429b10451876934ac9b9dea184156a3c3569
Jefferson Douglas <jedouglas.study@gmail.com>
2025-04-29 23:47:37 -0300

    Adicionando resolução da fase 02

---
+++ 2024/fase02/cubo_preto.py
@@ -0,0 +1,10 @@
+N = int(input())
+zero_faces = (N-2)**3
+uma_face = 6 * (N-2)**2
+duas_faces = 12 * (N-2)
+tres_faces = 8
+
+print(zero_faces)
+print(uma_face)
+print(duas_faces)
+print(tres_faces)
